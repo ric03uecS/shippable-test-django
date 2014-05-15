8dc0520b2cc5f25f6f1a44018ffd9c30f82b935f
Henrik Ossipoff Hansen <henrik.ossipoff@gmail.com>
2014-05-15 21:00:27 +0200

    1

---
+++ firstapp/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+class FirstAppTestCase(TestCase):
+    def test_first_test(self):
+        self.assertEqual(True, True)
+
+    def test_first_test(self):
+        self.assertEqual(True, False)
