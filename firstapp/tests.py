be94e7c7d0a81927c8b4157766007e376662bd52
Henrik Ossipoff Hansen <hoha@entertainment-trading.com>
2014-05-16 20:03:44 +0200

    Shippable test

--- firstapp/tests.py
+++ firstapp/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 
 class FirstAppTestCase(TestCase):
-    def test_first_test(self):
+    def test_first_test_1(self):
         self.assertEqual(True, True)
 
-    def test_first_test(self):
+    def test_first_test_2(self):
         self.assertEqual(2, 2)
